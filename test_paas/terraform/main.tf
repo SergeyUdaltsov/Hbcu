6b31a9678636ede907bf349e1f44fc3d32d23072
Serhii_Udaltsov <serhii_udaltsov@epam.com>
2021-10-06 12:44:44 +0300

    monitoring variable removed

--- test_paas/terraform/main.tf
+++ test_paas/terraform/main.tf
@@ -9,7 +9,6 @@ module "ec2_instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  monitoring             = var.monitoring
   vpc_security_group_ids = var.vpc_security_group_ids
   subnet_id              = var.subnet_id
   tags                   = var.tags
